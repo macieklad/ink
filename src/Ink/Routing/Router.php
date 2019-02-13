48daaf7ab3187c12861376ebafa203b299b139b5
Maciej Ładoś <macieklad@gmail.com>
2019-02-13 22:21:49 +0100

    Implemented router according to the tests

--- src/Ink/Routing/Router.php
+++ src/Ink/Routing/Router.php
@@ -4,23 +4,31 @@ namespace Ink\Routing;
 
 use Closure;
 use Ink\Routing\Route;
+use Ink\Container\ContainerProxy as Container;
 
 class Router 
 {
-    /**
-     * Possible http methods for the request
-     *
-     * @var array
-     */
-    protected $verbs = ['GET', 'POST'];
-
-
     /**
      * Array of attribute arrays to merge onto routes
      *
      * @var array
      */
     protected $attributeStack = [];
+
+    /**
+     * Route array
+     * 
+     * @var array
+     */
+    protected $routes = [];
+
+    /**
+     * Default controller namespace, from where they are
+     * resolved when passed as action string
+     * 
+     * @var string
+     */
+    protected $controllerNamespace = '';
 
     /**
      * Register GET request route
@@ -33,7 +41,42 @@ class Router
     {
         $route = $this->createRoute(['GET'], $uri, $attributes);
     }
+    /**
+     * Register POST request route
+     *
+     * @param string $uri
+     * @param mixed $action
+     * @return void
+     */
+    public function post(string $uri, $attributes)
+    {
+        $route = $this->createRoute(['POST'], $uri, $attributes);
+    }
 
+    /**
+     * Register PATCH request route
+     *
+     * @param string $uri
+     * @param mixed $action
+     * @return void
+     */
+    public function patch(string $uri, $attributes)
+    {
+        $route = $this->createRoute(['PATCH'], $uri, $attributes);
+    }
+
+    /**
+     * Register DELETE request route
+     *
+     * @param string $uri
+     * @param mixed $action
+     * @return void
+     */
+    public function delete(string $uri, $attributes)
+    {
+        $route = $this->createRoute(['DELETE'], $uri, $attributes);
+    }
+    
     /**
      * Create new route and add it to the register
      *
@@ -69,6 +112,8 @@ class Router
         if ($routes instanceof Closure) {
             $routes($this);
         } else {
+            $router = $this;
+
             require $routes;
         }
 
@@ -99,25 +144,110 @@ class Router
         }
 
         $route->prepare();
-        $this->registerRoute($route);
+        array_push($this->routes, $route);
     }
 
     /**
-     * Register WP_API route
-     *  
-     * @param \Stamp\Http\Route $route
+     * Load routes into wordpress
+     *
      * @return void
      */
-    public function registerRoute(Route $route)
+    public function listen()
     {
-        add_action('rest_api_init', function () use ($route) {
-            register_rest_route('v1', $route->wpUri, array(
-                'methods' => $route->methods,
-                'callback' => function ($data) {
-                    return 'Hello';
-                },
-            ));
-        });   
+        add_action('rest_api_init', function () {
+            foreach ($this->routes() as $route) {
+                register_rest_route($route->module, $route->wpUri, [
+                    'methods' => $route->methods,
+                    'callback' => $this->compileAction($route->action)
+                ]);
+            }
+        });           
+    }
+
+    /**
+     * Compile action used in route into a callback
+     * which can be executed by wordpress
+     *
+     * @param Closure|string $action
+     * @throws InvalidArgumentException
+     * @return Closure
+     */
+    public function compileAction($action): Closure
+    {
+        if (is_string($action)) {
+            return $this->compileStringAction($action);
+        }
+
+        if ($action instanceof Closure) {
+            return $this->compileCallbackAction($action);
+        }
+
+        throw new \InvalidArgumentException('Route action could not be compiled, as it is not string or callback, please fix it');
+    }
+
+    /**
+     * Compile action passed as string into a callback
+     *
+     * @param string $action
+     * @throws InvalidArgumentException
+     * @return void
+     */
+    protected function compileStringAction(string $action): Closure 
+    {
+        $actionParts = explode('@', $action);
+
+
+        if (count($actionParts) < 2) {
+            throw new \InvalidArgumentException("Provided action {$action} is not valid, we couldn't extract controller and method parts from it. Ensure it is in Controller@action format");
+        }
+
+        $controller = $this->controllerNamespace . '\\' . $actionParts[0];
+
+        if (! \class_exists($controller)) {
+            throw new \InvalidArgumentException("Class {$controller} provided to the action as Controller does not exist ! Specify a valid one, maybe it's a typo");
+        }
+
+        $method = $actionParts[1];
+        
+        return function ($req = null) use ($controller, $method) {
+            return Container::getInstance()->call([$controller, $method], [
+                'req' => $req
+            ]);
+        };
+    }
+
+    /**
+     * Compile action passed as callback
+     *
+     * @param Closure $action
+     * @return Closure
+     */
+    protected function compileCallbackAction(Closure $action): Closure
+    {
+        return function ($req = null) use ($action) {
+            return Container::getInstance()->call($action, [
+                'req' => $req
+            ]);
+        };
+    }
+
+    /**
+     * Set the base namespace, from where controllers will be loaded
+     * 
+     */
+    public function setControllerNamespace(string $namespace)
+    {
+        $this->controllerNamespace = $namespace;
+    }
+
+    /**
+     * Return registered routes
+     *
+     * @return void
+     */
+    public function routes()
+    {
+        return $this->routes;
     }
 
     /**
