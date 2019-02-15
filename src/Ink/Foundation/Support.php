e390edf0a8d294775e12b61ea87ac7b7bbb04928
Maciej Ładoś <macieklad@gmail.com>
2019-02-15 18:50:12 +0100

    Moved support class to foundation namespace, as it's core of stamp

---
+++ src/Ink/Foundation/Support.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Ink\Foundation;
+
+class Support
+{
+    /**
+     * Join many paths into single string
+     *
+     * @return void
+     */
+    public static function joinPaths()
+    {
+        $paths = array();
+
+        foreach (func_get_args() as $arg) {
+            if ($arg !== '') {
+                $paths[] = $arg;
+            }
+        }
+
+        return preg_replace('#' . DIRECTORY_SEPARATOR . '+#', DIRECTORY_SEPARATOR, join(DIRECTORY_SEPARATOR, $paths));
+    }
+
+}
