d0fcd7daa6264c612588a4c539b0ef523835535c
macieklad <macieklad@gmail.com>
2019-02-22 14:27:20 +0100

    Updated globals for filter functions

--- tests/Hooks/globals.php
+++ tests/Hooks/globals.php
@@ -77,3 +77,74 @@ function remove_all_actions()
     return TestHelpers::passGlobalCall('remove_all_actions', $args);
 }
 
+/**
+ * Mock wordpress has_filter
+ *
+ * @return void
+ */
+function has_filter()
+{
+    $args = \func_get_args();
+
+    return TestHelpers::passGlobalCall('has_filter', $args);
+}
+
+/**
+ * Mock wordpress add_filter
+ *
+ * @return void
+ */
+function add_filter()
+{
+    $args = \func_get_args();
+
+    return TestHelpers::passGlobalCall('add_filter', $args);
+}
+
+/**
+ * Mock wordpress apply_filters
+ *
+ * @return void
+ */
+function apply_filters()
+{
+    $args = \func_get_args();
+
+    return TestHelpers::passGlobalCall('apply_filters', $args);
+}
+
+/**
+ * Mock wordpress remove_filter
+ *
+ * @return void
+ */
+function remove_filter()
+{
+    $args = \func_get_args();
+
+    return TestHelpers::passGlobalCall('remove_filter', $args);
+}
+
+/**
+ * Mock wordpress remove_all_filters
+ *
+ * @return void
+ */
+function remove_all_filters()
+{
+    $args = \func_get_args();
+
+    return TestHelpers::passGlobalCall('remove_all_filters', $args);
+}
+
+/**
+ * Mock wordpress doing_filter
+ *
+ * @return void
+ */
+function doing_filter()
+{
+    $args = \func_get_args();
+
+    return TestHelpers::passGlobalCall('doing_filter', $args);
+}
